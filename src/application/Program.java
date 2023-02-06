69e2f7121e59c7f956e7d048f210db27446363f6
Aécio Guterres <aecioguterres@gmail.com>
2023-02-06 16:44:03 -0300

    Implementando findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,7 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
+
 import model.entities.Seller;
 
 import java.util.Date;
@@ -11,11 +11,9 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-
-        Seller seller = new Seller(2, "aecio", "aecioguterres@gmail.com", new Date(), 3900.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
 
